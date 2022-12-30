e5471d346a50b207de6dd37ffc9e5b2a94df0935
Ioan Sava <ioan.sava28@gmail.com>
2022-12-30 17:43:55 +0200

    Get cryptocurrency by id

--- cryptocurrencies-service/src/envs.ts
+++ cryptocurrencies-service/src/envs.ts
@@ -1,8 +1,9 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 
 config();
 
 export default {
     port: process.env.PORT || 3000,
-    graphqlPath: process.env.GRAPHQL_PATH || '/graphql'
+    graphqlPath: process.env.GRAPHQL_PATH || "/graphql",
+    sparqlEndpoint: process.env.SPARQL_ENDPOINT || "http://localhost:9999/blazegraph/sparql"
 }
