14f05fbb41abf6172e85979ef14d5571cc50b448
Diana Crainic <dianacrainic801@yahoo.com>
2023-01-11 21:08:46 +0200

    remove unused libraries, add fixes

--- cryk-ui/src/pages/CryptoInformation.tsx
+++ cryk-ui/src/pages/CryptoInformation.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import NewsCard from "../components/NewsCard";
 import "./css/CryptoInformation.css";
