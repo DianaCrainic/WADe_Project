8575adaed806b4f473182856deab68d29ea80b56
Ioan Sava <ioan.sava28@gmail.com>
2023-02-04 21:09:40 +0200

    Add script for crypto prices data saving

---
+++ scripts/save-crypto-prices-data.ts
@@ -0,0 +1,64 @@
+import ParsingClient from "sparql-http-client/ParsingClient";
+import axios from "axios";
+
+const SPARQL_ENDPOINT = process.env.SPARQL_ENDPOINT || "http://localhost:9999/blazegraph/sparql";
+const sparqlClient = new ParsingClient({ endpointUrl: SPARQL_ENDPOINT, updateUrl: SPARQL_ENDPOINT });
+
+const COIN_GECKO_API_URL = "https://api.coingecko.com/api/v3/";
+
+const getCryptocurrencyMarketData = async (name: string, startDate: number, endDate: number): Promise<any> => {
+    try {
+        const range = `range?vs_currency=usd&from=${startDate}&to=${endDate}`;
+        const { data, status } = await axios.get<any>(
+            `${COIN_GECKO_API_URL}/coins/${name}/market_chart/${range}`,
+            {
+                headers: {
+                    Accept: "application/json",
+                },
+            },
+        );
+
+        console.log(`Response status is: ${status}`);
+
+        return data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error(error.message);
+            return error.message;
+        } else {
+            console.error(error);
+            return "An unexpected error occurred";
+        }
+    }
+}
+
+const main = async () => {
+    const name = "ethereum"; // insert cryptocurrency name
+    const startDate = new Date(new Date().setFullYear(new Date().getFullYear() - 3)).getTime() / 1000; // 3 years ago
+    const endDate = new Date().getTime() / 1000; // now
+    const cryptoMarketData = await getCryptocurrencyMarketData(name, startDate, endDate);
+
+    const cryptocurrencyId = "http://purl.org/net/bel-epa/doacc#D09fe733c-bb49-4854-a6d2-0c9cf77fa545"; // insert cryptocurrency id
+    if (cryptoMarketData && cryptoMarketData.prices && cryptoMarketData.prices.length > 0) {
+        for (let i = 0; i < cryptoMarketData.prices.length; ++i) {
+            const priceDataId = `http://purl.org/net/bel-epa/doacc#${crypto.randomUUID()}`;
+            const query = `
+                PREFIX doacc:  <http://purl.org/net/bel-epa/doacc#>
+                PREFIX schema: <http://schema.org/>
+                PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+                INSERT DATA {
+                    <${cryptocurrencyId}> doacc:hadPrice    <${priceDataId}>                                                                .
+                    <${priceDataId}>      rdf:type          doacc:PriceData                                                                 ;
+                                          schema:currency   "USD"^^<http://www.w3.org/2001/XMLSchema#string>                                ;
+                                          schema:datePosted "${cryptoMarketData.prices[i][0]}"^^<http://www.w3.org/2001/XMLSchema#integer>  ;
+                                          schema:value      "${cryptoMarketData.prices[i][1]}"^^<http://www.w3.org/2001/XMLSchema#numeric>  .
+                }
+            `;
+
+            await sparqlClient.query.update(query);
+        }
+    }
+}
+
+main();
