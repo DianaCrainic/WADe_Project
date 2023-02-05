999faae8e99fe24fbabae776ed0f9857bf936f3c
Ioan Sava <ioan.sava28@gmail.com>
2023-02-06 00:36:52 +0200

    improve save crypto prices data script and add readme

--- scripts/save-crypto-prices-data.ts
+++ scripts/save-crypto-prices-data.ts
@@ -34,7 +34,7 @@ const getCryptocurrencyMarketData = async (name: string, startDate: number, endD
 
 const main = async () => {
     const name = "ethereum"; // insert cryptocurrency name
-    const startDate = new Date(new Date().setFullYear(new Date().getFullYear() - 3)).getTime() / 1000; // 3 years ago
+    const startDate = new Date(new Date().setFullYear(new Date().getFullYear() - 5)).getTime() / 1000; // 5 years ago
     const endDate = new Date().getTime() / 1000; // now
     const cryptoMarketData = await getCryptocurrencyMarketData(name, startDate, endDate);
 
