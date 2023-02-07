4c5b3f86d7b5eaf9cca5c370feac2d6bc77dca53
Ioan Sava <ioan.sava28@gmail.com>
2023-02-07 13:06:53 +0200

    Add filters for DCA chart

--- cryk-ui/src/components/DCAChart.tsx
+++ cryk-ui/src/components/DCAChart.tsx
@@ -1,8 +1,14 @@
+import TextField from "@mui/material/TextField";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import React, { useEffect, useState } from "react";
 import { Area, AreaChart, Legend, Tooltip, XAxis, YAxis } from "recharts";
 import { PriceData } from "../models/PriceData";
 import Calculation from "./Calculation";
 import "./css/DCAChart.css";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
+
+const DEFAULT_AMOUNT_TO_INVEST_VALUE = 5;
 
 type DCACalculations = {
     endTotal: number,
@@ -12,16 +18,21 @@ type DCACalculations = {
     totalPercentGained: number,
 };
 
-const getTotalCalculations = (priceHistory: PriceData[], amountToInvest: number): DCACalculations => {
-    const numberOfDays = priceHistory.length;
+const getTotalCalculations = (priceHistory: PriceData[], amountToInvest: number, startDate: Dayjs | null, endDate: Dayjs | null): DCACalculations => {
+    let numberOfDays = 0;
     let cryptocurrencyAmount = 0;
-    for (let i = 0; i < numberOfDays; ++i) {
-        cryptocurrencyAmount += amountToInvest / priceHistory[i].value;
+    for (let i = 0; i < priceHistory.length; ++i) {
+        const timestamp = priceHistory[i].timestamp;
+        if ((startDate == null || startDate.unix() * 1000 <= timestamp) && (endDate == null || endDate.unix() * 1000 >= timestamp)) {
+            const cryptocurrencyPrice = priceHistory[i].value;
+            cryptocurrencyAmount += amountToInvest / cryptocurrencyPrice;
+            ++numberOfDays;
+        }
     }
 
     const totalCoinAmount = cryptocurrencyAmount;
     const totalInvested = amountToInvest * numberOfDays;
-    const endTotal = totalCoinAmount * priceHistory[priceHistory.length - 1].value;
+    const endTotal = numberOfDays > 0 ? totalCoinAmount * priceHistory[numberOfDays - 1].value : 0;
     const totalGained = endTotal - totalInvested;
     const totalPercentGained = ((endTotal - totalInvested) / totalInvested) * 100;
 
@@ -34,36 +45,41 @@ const getTotalCalculations = (priceHistory: PriceData[], amountToInvest: number)
     };
 };
 
-const getDailyCalculations = (priceHistory: PriceData[], amountToInvest: number): any[] => {
-    const numberOfDays = priceHistory.length;
+const getDailyCalculations = (priceHistory: PriceData[], amountToInvest: number, startDate: Dayjs | null, endDate: Dayjs | null): any[] => {
     let dailyCalculations = [];
     let cryptocurrencyAmount = 0;
     let totalInvested = 0;
 
-    for (let i = 0; i < numberOfDays; ++i) {
-        const cryptocurrencyPrice = priceHistory[i].value;
-        cryptocurrencyAmount += amountToInvest / cryptocurrencyPrice;
-        totalInvested += amountToInvest;
-        const total = (cryptocurrencyAmount * cryptocurrencyPrice);
-        const date = new Date(priceHistory[i].timestamp);
-        const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    for (let i = 0; i < priceHistory.length; ++i) {
+        const timestamp = priceHistory[i].timestamp;
+        if ((startDate == null || startDate.unix() * 1000 <= timestamp) && (endDate == null || endDate.unix() * 1000 >= timestamp)) {
+            const cryptocurrencyPrice = priceHistory[i].value;
+            cryptocurrencyAmount += amountToInvest / cryptocurrencyPrice;
+            totalInvested += amountToInvest;
+            const total = (cryptocurrencyAmount * cryptocurrencyPrice);
+            const date = new Date(timestamp);
+            const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 
-        dailyCalculations.push({
-            totalInvested,
-            cryptocurrencyAmount,
-            cryptocurrencyPrice,
-            total,
-            date: formattedDate,
-        });
+            dailyCalculations.push({
+                totalInvested,
+                cryptocurrencyAmount,
+                cryptocurrencyPrice,
+                total,
+                date: formattedDate,
+            });
+        }
     }
 
     return dailyCalculations;
 };
 
 export default function DCAChart(props: { priceHistory: PriceData[], cryptocurrencySymbol: string | undefined }) {
-    const [amountToInvest, setAmountToInvest] = useState<number>(200);
+    const [sortedAndFilteredPriceHistory, setSortedAndFilteredPriceHistory] = useState<PriceData[]>([]);
+    const [amountToInvest, setAmountToInvest] = useState<number>(DEFAULT_AMOUNT_TO_INVEST_VALUE);
     const [calculations, setCalculations] = useState<DCACalculations>();
     const [dailyCalculations, setDailyCalculations] = useState<any[]>([]);
+    const [startDate, setStartDate] = React.useState<Dayjs | null>(null);
+    const [endDate, setEndDate] = React.useState<Dayjs | null>(null);
 
     const [isTotalPortfolioValueAreaDisplayed, setIsTotalPortfolioValueAreaDisplayed] = useState(true);
     const [isAmountOfCryptocurrencyOwnedAreaDisplayed, setIsAmountOfCryptocurrencyOwnedAreaDisplayed] = useState<boolean>(true);
@@ -72,9 +88,33 @@ export default function DCAChart(props: { priceHistory: PriceData[], cryptocurre
 
     useEffect(() => {
         const sortedPriceHistoryByTimestamp = [...props.priceHistory].sort((pd1, pd2) => pd1.timestamp - pd2.timestamp);
-        setCalculations(getTotalCalculations(sortedPriceHistoryByTimestamp, amountToInvest));
-        setDailyCalculations(getDailyCalculations(sortedPriceHistoryByTimestamp, amountToInvest));
-    }, [props.priceHistory, amountToInvest]);
+        let filteredPriceHistory: PriceData[] = []; // keep only one price data per day
+        let previousTimestamp = null;
+        for (let i = 0; i < sortedPriceHistoryByTimestamp.length; ++i) {
+            if (
+                previousTimestamp == null ||
+                (new Date(previousTimestamp).getDay() !== new Date(sortedPriceHistoryByTimestamp[i].timestamp).getDay())
+            ) {
+                previousTimestamp = sortedPriceHistoryByTimestamp[i].timestamp;
+                filteredPriceHistory.push(sortedPriceHistoryByTimestamp[i]);
+            }
+        }
+        setSortedAndFilteredPriceHistory(filteredPriceHistory);
+    }, [props.priceHistory]);
+
+    useEffect(() => {
+        setCalculations(getTotalCalculations(sortedAndFilteredPriceHistory, amountToInvest, startDate, endDate));
+        setDailyCalculations(getDailyCalculations(sortedAndFilteredPriceHistory, amountToInvest, startDate, endDate));
+    }, [sortedAndFilteredPriceHistory, amountToInvest, startDate, endDate]);
+
+    const onAmountToInvestChange = (event: any) => {
+        const inputValue = event.target.value;
+        if (inputValue === "") {
+            setAmountToInvest(0);
+        } else {
+            setAmountToInvest(Number.parseFloat(inputValue));
+        }
+    };
 
     const onClickHandlerLegend = (event: any) => {
         if (event.dataKey === "total") {
@@ -89,15 +129,48 @@ export default function DCAChart(props: { priceHistory: PriceData[], cryptocurre
         else if (event.dataKey === "cryptocurrencyPrice") {
             setIsCryptocurrencyPriceAreaDisplayed(!isCryptocurrencyPriceAreaDisplayed);
         }
-    }
+    };
 
     return (
         <div className="dca-chart-container">
             <h2 className="dca-chart-title">DCA Chart</h2>
             <p className="dca-description">Dollar-cost averaging (DCA) is an investment strategy in which the intention is to minimize the impact of volatility when investing or purchasing a large block of a financial asset or instrument. It is also called unit cost averaging, incremental averaging, or cost average effect.</p>
+            <div className="dca-chart-filters-container">
+                <TextField
+                    variant="standard"
+                    label="Amount to invest (USD)"
+                    type="number"
+                    size="medium"
+                    defaultValue={DEFAULT_AMOUNT_TO_INVEST_VALUE}
+                    InputProps={{ inputProps: { min: 0 } }}
+                    onChange={onAmountToInvestChange}
+                />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label="Start date"
+                        value={startDate}
+                        onChange={(newDate) => {
+                            setStartDate(newDate);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                        maxDate={endDate ? endDate : dayjs()}
+                    />
+                </LocalizationProvider>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label="End date"
+                        value={endDate}
+                        onChange={(newDate) => {
+                            setEndDate(newDate);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                        minDate={startDate ? startDate : dayjs()}
+                    />
+                </LocalizationProvider>
+            </div>
             <div className="calculations-container">
                 <Calculation
-                    description={`End total (USD)`}
+                    description={"End total (USD)"}
                     value={`$${calculations?.endTotal.toFixed(2)}`}
                 />
                 <Calculation
@@ -105,15 +178,15 @@ export default function DCAChart(props: { priceHistory: PriceData[], cryptocurre
                     value={`${calculations?.totalCoinAmount.toFixed(5)}`}
                 />
                 <Calculation
-                    description={`Amount invested (USD)`}
+                    description={"Amount invested (USD)"}
                     value={`$${calculations?.totalInvested.toFixed(2)}`}
                 />
                 <Calculation
-                    description={`Gained (USD)`}
-                    value={`$${calculations?.totalGained.toFixed(2)}`}
+                    description={"Gained (USD)"}
+                    value={`${calculations?.totalGained.toFixed(2)}`}
                 />
                 <Calculation
-                    description={`Gained (%)`}
+                    description={"Gained (%)"}
                     value={`${calculations?.totalPercentGained.toFixed(2)}%`}
                 />
             </div>
